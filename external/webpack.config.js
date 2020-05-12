681f9b285c9864a0b9341d8b6423c9e59243d96f
sue <sue@MacBook-Pro-de-sue.local>
2020-05-12 00:50:12 -0500

    loading set up by default and with personal config

--- external/webpack.config.js
+++ external/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry:'./index.js',
+  entry: path.resolve(__dirname,'index.js'),
   mode: 'development',
   output: {
     path: path.resolve(__dirname,'dist'),
     filename:'bundle.js',
   }
 }
- 
+
