c2c2611f5baea20c4f304923f154a37bf6cbe97c
sue <sue@MacBook-Pro-de-sue.local>
2020-05-12 02:33:27 -0500

    teasting loaders

--- multi-entry-points/webpack.config.js
+++ multi-entry-points/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    home: path.resolve(__dirname,'src/js/index.js'),
+    home: path.resolve(__dirname, "src/js/index.js"),
     prices: path.resolve(__dirname,'src/js/prices.js'),
     contact: path.resolve(__dirname,'src/js/contact.js'),
   },
