852b9265ccaf092e9aa11be0b55f80194bb4bd5d
sue <sue@MacBook-Pro-de-sue.local>
2020-05-14 00:29:39 -0500

    setting up React

---
+++ react/src/js/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      que rete bonito es hacer apps con React.js
+    </div>
+  )
+}
+
+export default App;
---
+++ react/src/js/index.js
@@ -0,0 +1,5 @@
+import '../css/index.css'
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App/>, document.getElementById('container'));
