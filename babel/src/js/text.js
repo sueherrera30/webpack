64b05b2b0f25a82113b7b2c866bb3dd1c951bebe
sue <sue@MacBook-Pro-de-sue.local>
2020-05-13 22:19:55 -0500

    api fetch excercise

---
+++ babel/src/js/text.js
@@ -0,0 +1,5 @@
+const text = () => {
+  document.body.innerHTML = '<p> test 5 again Si ves este texto es que ya anda jalando esta cosa';
+}
+
+export default text;
---
+++ babel/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const webpack = require('webpack');
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, "src/js/index.js"),
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename:'js/[name].js',
+  },
+  devServer: {
+    hot: true,
+    open:true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      // dentro de reglas agregamos un loader nuevo
+      {
+        test: /\.js$/,
+        // si usas mas puedes usar corchetes.
+        // asi comop esta, funciona, pero puedo agregarle reglas extras.
+        //como sotortar react vue etc, pero mejor 
+        //guardar en archivo propio: .babelrc, recibira un json.
+        use: 'babel-loader',
+        // no quiero que a estos no les agregues babel, como react modules.
+        exclude: /node_modules/,
+      },
+      {
+        test:/\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack-dev-server',
+
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+}
+
